859bdc5fda0cb8f70d98b957b9ec9b954767d5ce
Eduardo Nunes <eduardo.afonso.silva@ccc.ufcg.edu.br>
2024-03-07 13:56:26 -0300

    removing list-item mocks

--- scripts/index.mjs
+++ scripts/index.mjs
@@ -1,34 +1,3 @@
-// const slider = document.querySelectorAll('.slider');
-// const prevBtn = document.getElementById('prev-btn');
-// const nextBtn = document.getElementById('next-btn');
-
-
-// let currentSlide = 0;
-
-// function hideSlide(){
-//     slider.forEach(item => item.classList.remove('on'));    
-// }
-
-// function showSlide(){
-//     slider[currentSlide].classList.add('on');
-// }
-
-// function nextSlide(){
-//     hideSlide();
-//     currentSlide = slider.length - 1 === currentSlide ? 0 : currentSlide + 1;
-//     showSlide();
-// }
-
-// function prevSlide(){
-//     hideSlide();
-//     currentSlide = currentSlide === 0 ? slider.length - 1 : currentSlide - 1;
-//     showSlide();
-// }
-
-// nextBtn.addEventListener('click', nextSlide)
-
-// prevBtn.addEventListener('click', prevSlide)
-
 const listOfCardElements = document.querySelectorAll('.card');
 const cardContainer = document.querySelector('.carousel');
 
